720196be59f3839f9f576fe6751f1b9f3959f169
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-24 01:05:18 +0530

    Tour

--- src/components/Tour/index.js
+++ src/components/Tour/index.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-function index() {
-  return <div>Hello from tour list</div>;
+export class index extends Component {
+  render() {
+    const { img, id, city, name, info } = this.props.tour;
+    const { removeTouur } = this.props;
+    return (
+      <article className="tour">
+        <div className="img-container">
+          <img src={img} alt={name} />
+          <span>
+            <i className="fas fa-window-close"></i>
+          </span>
+        </div>
+        <div className="tour-info">
+          <h3>{city}</h3>
+          <h4>{name}</h4>
+          <h5>
+            info
+            <span>
+              <i class="fas fa-carrot-sqaure-down"></i>
+            </span>
+          </h5>
+          <p>{info}</p>
+        </div>
+      </article>
+    );
+  }
 }
 
 export default index;
